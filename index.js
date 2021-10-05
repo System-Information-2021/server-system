1c4dc11ab2c978e09986ad3dfea75abfe0f54958
Nguyen Tran Hoang An <hoangan726@gmail.com>
2021-10-05 22:11:39 +0700

    Update port in file index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 // const port = 3000
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is listening at http://localhost:${process.env.PORT || 3000}`)
 })
 
