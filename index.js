159ec1b7450b968cfbc78ee300dfc8c4cc72be5b
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2021-10-09 21:49:24 +0700

    add cor

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const app = express()
 require('dotenv').config()
 
 // cor
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
-
+app.use(cors({
+  origin: '*'
+}))
 
 
 // Define routes 
