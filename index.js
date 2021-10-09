1933df39500b34611361c8ef91ca03af13db791a
Nguyen Tran Hoang An <hoangan726@gmail.com>
2021-10-09 17:40:20 +0700

    Update user model, controller and route path

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const { Client, Pool } = require('pg')
 require('dotenv').config()
 
 
@@ -13,24 +12,6 @@ app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.use('/user', userRoute)
-
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PBPORT,
-  ssl: { rejectUnauthorized: false }
-})
-
-pool.connect(err => {
-  if (err) {
-    console.error('error connecting', err.stack)
-  } else {
-    console.log('connected')
-    pool.end()
-  }
-})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening at http://localhost:${process.env.PORT}`)
--- src/route/user.route.js
+++ src/route/user.route.js
@@ -4,9 +4,8 @@ const controller = require('../controller/user.controller')
 const router = express.Router()
 
 router.get('/',controller.getAllUsers)
-router.post('/create', controller.createUser)
+router.post('/register', controller.createUser)
 router.post('/login', controller.authentication)
-router.get('/auth', controller.authorization , controller.getAllUsers)
+router.post('/auth', controller.authorization , controller.getAllUsers)
 
 module.exports = router;
-
