54fbb5a67f22d115e5d2f245e8512d071f431d14
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2021-10-17 01:18:45 +0700

    change token

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -24,22 +24,22 @@ const ValidatePhone = (phone) => {
 const getAllUsers = (req, res) => {
 
     User.findAll()
-    .then(listUsers =>{
-        listUsers.password = undefined;
-        res.status(200).json({
-            status: 'OK',
-            code: 200,
-            listUsers,
+        .then(listUsers => {
+            listUsers.password = undefined;
+            res.status(200).json({
+                status: 'OK',
+                code: 200,
+                listUsers,
+            });
+        })
+        .catch(Error => {
+            if (!Error.status) {
+                res.json({
+                    message: 'Something went wrong!'
+                })
+            }
+            next(Error);
         });
-    })
-    .catch(Error =>{
-        if(! Error.status){
-            res.json({
-                message: 'Something went wrong!'
-            })
-        }
-        next(Error);
-    });
 }
 
 
@@ -55,15 +55,15 @@ const register = async (req, res, next) => {
         comment,
         address,
         city } = req.body;
-    
-        
-                // check email exist
-        const checkmail = await User.findOne({
-                            where: {
-                            email: email
-                            }
-                        });
-           
+
+
+    // check email exist
+    const checkmail = await User.findOne({
+        where: {
+            email: email
+        }
+    });
+
     if (!password || !email) {
         res.json({
             status: 'Bad Request',
@@ -78,72 +78,72 @@ const register = async (req, res, next) => {
             error.push('Wrong email format')
         }
         // Check existence of email input
-        if(checkmail == null){
+        if (checkmail == null) {
             // Validate re-password of end-user
-                if (!re_password) {
-                    error.push('Enter the re-password')
-                } else if (re_password !== password) {
-                    error.push('Password does not match')
+            if (!re_password) {
+                error.push('Enter the re-password')
+            } else if (re_password !== password) {
+                error.push('Password does not match')
+            }
+
+            // Hash password processing
+            const hash = await bcrypt.hash(password, 8)
+
+            // Initialize User object
+            const user = new User({
+                password: hash,
+                email: email,
+                isadmin: false,
+            })
+            // Check if input has number phone and validate their format 
+            if (mobile_number) {
+                var check_phone = ValidatePhone(mobile_number);
+                if (!check_phone) {
+                    error.push('Enter phone again')
+                } else {
+                    user.mobile_number = mobile_number;
                 }
+            }
 
-                // Hash password processing
-                const hash = await bcrypt.hash(password, 8)
+            if (firstname && lastname) {
+                user.firstname = firstname;
+                user.lastname = lastname;
+            }
 
-                // Initialize User object
-                const user = new User({
-                    password: hash,
-                    email: email,
-                    isadmin: false,
+            if (company) {
+                user.company = company;
+            }
+            // Check if input has address value, the city field must be enter following
+            if (address) {
+                if (!city) {
+                    error.push('No city')
+                } else {
+                    user.address = address;
+                    user.city = city;
+                }
+            }
+
+            if (comment) {
+                user.comment = comment;
+            }
+            // If any validations above has error, the function will return those errors and stop immediately
+            if (error.length !== 0) {
+                return res.json({
+                    status: 'Bad Request',
+                    code: 400,
+                    message: error
                 })
-                // Check if input has number phone and validate their format 
-                if (mobile_number) {
-                    var check_phone = ValidatePhone(mobile_number);
-                    if (!check_phone) {
-                        error.push('Enter phone again')
-                    } else {
-                        user.mobile_number = mobile_number;
-                    }
-                }
+            }
 
-                if (firstname && lastname) {
-                    user.firstname = firstname;
-                    user.lastname = lastname;
-                }
-
-                if (company) {
-                    user.company = company;
-                }
-                // Check if input has address value, the city field must be enter following
-                if (address) {
-                    if (!city) {
-                        error.push('No city')
-                    } else {
-                        user.address = address;
-                        user.city = city;
-                    }
-                }
-
-                if (comment) {
-                    user.comment = comment;
-                }
-                // If any validations above has error, the function will return those errors and stop immediately
-                if (error.length !== 0) {
-                    return res.json({
-                        status: 'Bad Request',
-                        code: 400,
-                        message: error
-                    })
-                }
-
-                user.save()
-                .then(results =>{
-                    if(results !== null){           
-                    res.json({
+            user.save()
+                .then(results => {
+                    if (results !== null) {
+                        res.json({
                             status: 'Created',
                             code: 200,
                             message: 'Successully'
 
-                     })
+                        })
                     }
                     else {
                         res.json({
@@ -153,14 +153,14 @@ const register = async (req, res, next) => {
                         })
                     }
                 })
-                .catch(err =>{
-                    if(!err.statusCode){
+                .catch(err => {
+                    if (!err.statusCode) {
                         err.statusCode = 500;
                     }
                     next(err);
                 });
-            
-       
+
+
         }
         else {
             // If email does exist in db, return the message for ui and asks user entering again 
@@ -176,7 +176,7 @@ const register = async (req, res, next) => {
 
 
 
-        //
+    //
 };
 
 const authentication = async (req, res) => {
@@ -188,57 +188,57 @@ const authentication = async (req, res) => {
             message: 'Please fill all fields'
         })
     }
-               // check email exist
-               var checkmail = await User.findOne({
-                where: {
-                email: email
-                },
-               
-            });
-        
-            
+    // check email exist
+    var checkmail = await User.findOne({
+        where: {
+            email: email
+        },
+
+    });
+
+
     try {
         if (ValidateEmail(email)) {
             //KT xem cai email hay khong
-            if(checkmail == null){
+            if (checkmail == null) {
+                return res.json({
+                    status: 'Bad Request',
+                    code: 400,
+                    message: 'User does not exist'
+                });
+            }
+            else {
+                const matchPassword = await bcrypt.compare(password, checkmail.password);
+                if (matchPassword) {
+                    // Initialize token string to identify user when they login
+                    var token =
+                        jwt.sign({
+                            id: User.id,
+                            email: User.email
+                        }, process.env.SECRET_KEY, { expiresIn: 60 });
+                    // luu token
+                    await checkmail.update({ token: token });
+                    return res.json({
+                        status: "OK",
+                        code: 200,
+                        token: token,
+                        creat_at: new Date().toLocaleString(),
+                        expire_at: new Date(Date.now() + 60000).toLocaleString()
+                    })
+
+
+
+
+                    //
+                } else if (!matchPassword) {
                     return res.json({
                         status: 'Bad Request',
                         code: 400,
-                        message: 'User does not exist'
-                    });
+                        message: 'Wrong Password'
+                    })
                 }
-                 else {
-                    const matchPassword = await bcrypt.compare(password, checkmail.password);                 
-                    if (matchPassword) {
-                        // Initialize token string to identify user when they login
-                        var token =
-                            jwt.sign({
-                                id: User.id,
-                                email: User.email
-                            }, process.env.SECRET_KEY, { expiresIn: 60 });
-                        // luu token
-                        await checkmail.update({ token: token });
-                        return res.json({
-                            status: "OK",
-                            code: 200,
-                            token: token,
-                            creat_at: new Date().toLocaleString(),
-                            expire_at: new Date(Date.now() + 60000).toLocaleString()
-                        })
-                        
-                        
-                        
-                        
-                        //
-                    } else if (!matchPassword) {
-                        return res.json({
-                            status: 'Bad Request',
-                            code: 400,
-                            message: 'Wrong Password'
-                        })
-                    }
-                }
-            
+            }
+
         } else {
             return res.json({
                 status: 'Bad Request',
@@ -257,26 +257,29 @@ const authentication = async (req, res) => {
 }
 
 
-const getUserByToken = async(req,res)=>{
+const getUserByToken = async (req, res) => {
     const token = req.body.token;
     console.log(token);
-    const userToken =await  User.findOne({ where: { token: token } });
-            
-          if (userToken === null)
-          res.json({
-              code: 400,
-              status: 'invalided token'
-          })         
-          else {
-            userToken.password = undefined;
-            res.json({
-                code: 200,
-                status: 'OK',
-                data: userToken
-            })
-          }
-        
-        
+    const userToken = await User.findOne({ where: { token: token } });
+
+    if (userToken === null)
+        res.json({
+            code: 200,
+            status: 'OK',
+            data: {
+                tokenError: "not token for user"
+            }
+        })
+    else {
+        userToken.password = undefined;
+        res.json({
+            code: 200,
+            status: 'OK',
+            data: userToken
+        })
+    }
+
+
 }
 
 
