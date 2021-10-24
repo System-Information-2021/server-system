4afeb222bc0907fe972f6f7e7183cd38eb23cf2b
Nguyen Tran Hoang An <hoangan726@gmail.com>
2021-10-24 22:57:20 +0700

    Add middleware and add new api for customer

---
+++ src/route/customer.route.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const controller = require('../controller/product.controller')
+
+const router = express.Router()
+
+router.get('/product',controller.getAllProductForCustomer)
+router.get('/product/:id', controller.getProductById)
+
+module.exports = router;
--- src/route/products.route.js
+++ src/route/products.route.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const controller = require('../controller/product.controller')
 const upload = require('../../utils/upload')
+
 const router = express.Router()
 
-router.get('/customer',controller.getAllProductForCustomer)
 router.post('/add', upload.array('images', 3) , controller.addProduct)
 router.get('/:id', controller.getProductById)
 router.delete('/delete/:id', controller.deleteProduct)
