cd334bf6d687e2c168e62330f25fb2ffdf4822c1
Nguyen Tran Hoang An <hoangan726@gmail.com>
2021-10-26 15:15:14 +0700

    fix bug

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -3,257 +3,295 @@ const Brand = require('../model/brands.model')
 const Category = require('../model/categories.model')
 const path = require('path')
 
-const addProduct = async (req,res) => {
-    const { id_brand , id_category } = req.body
+const addProduct = async (req, res) => {
+    let { id_brand, id_category } = req.body
+    id_brand = parseInt(id_brand)
+    id_category = parseInt(id_category)
+
+    if (isNaN(id_brand) || isNaN(id_category)) {
+        return res.json({
+            code: 400,
+            status: 'Bad Request',
+            message: 'Brand or category have not been filled'
+        })
+    }
     try {
         const brand = await Brand.findByPk(id_brand)
         const category = await Category.findByPk(id_category)
-        if(brand !== null && category !== null) {
-            const { 
-                name, 
+        if (brand !== null && category !== null) {
+            const {
+                name,
                 price,
                 description
             } = req.body
-            const productMatch = await Product.findOne({where : { name : name }})
-            if(productMatch === null) {
-                const [{filename : image1} , {filename : image2}, {filename : image3} ] = req.files
+            const productMatch = await Product.findOne({ where: { name: name } })
+            if (productMatch === null) {
+                const [{ filename: image1 }, { filename: image2 }, { filename: image3 }] = req.files
                 const product = await Product.create({
-                    name : name,
-                    price : price,
-                    image1 : image1,
-                    image2 : image2,
-                    image3 : image3,
-                    description : description,
-                    id_brand : brand.id,
-                    id_category : category.id
+                    name: name,
+                    price: price,
+                    image1: image1,
+                    image2: image2,
+                    image3: image3,
+                    description: description,
+                    id_brand: brand.id,
+                    id_category: category.id
                 })
-    
+
                 return res.json({
-                    code : 200,
-                    status : 'Created',
-                    message : 'Successfully',
-                    newRecord : product
+                    code: 200,
+                    status: 'Created',
+                    message: 'Successfully',
+                    newRecord: product
                 })
             } else {
                 return res.json({
-                    code : 400,
-                    status : 'Bad Request',
-                    message : 'Product does exist'
-                   })
+                    code: 400,
+                    status: 'Bad Request',
+                    message: 'Product does exist'
+                })
             }
         } else {
             return res.json({
-                code : 400,
-                status : 'Bad Request',
-                message : 'Product must have both of the brand and the category'
+                code: 400,
+                status: 'Bad Request',
+                message: 'Product must have both of the brand and the category'
             })
         }
     } catch (err) {
-        let errors = []
-        
-        err.errors.forEach((each) => {
-            errors.push(each.message)
-        })
-        return res.json({
-            code : 400,
-            status : 'Bad Request',
-            message : errors
-        })
-    }
-}
-
-const getProductById = async (req,res) => {
-    try {
-        const { id } = req.params
-        const product = await Product.findOne({
-            where : { id : id },
-            include : { all : true }
-        })
-        product.id_category = undefined,
-        product.id_brand = undefined
-        return res.json({
-            code : 200,
-            status : 'OK',
-            data : product
-        })
-    } catch (err) {
-        res.json(err)
-    }
-}
-
-const deleteProduct = async (req,res) => {
-    try {
-        const id_product = req.params.id;
-        const existProduct = await Product.findByPk(id_product)
-        if(existProduct !== null) {
-            await existProduct.destroy()
+        if (err.errors) {
+            let errors = []
+            if (err.length > 1) {
+                err.errors.forEach((each) => {
+                    errors.push(each.message)
+                })
+            } else {
+                errors.push(err.errors[0].message)
+            }
             return res.json({
-                code : 200,
-                status : 'Deleted',
-                message : 'Successfully'
+                code: 400,
+                status: 'Bad Request',
+                message: errors
             })
         } else {
             return res.json({
-                code : 400,
-                status : 'Bad Request',
-                message : 'Product does not exist'
+                code: 500,
+                status: 'Internal Error',
+                message: 'Something went wrong'
+            })
+        }
+    }
+}
+
+const getProductById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findOne({
+            where: { id: id },
+            include: { all: true }
+        })
+        product.id_category = undefined,
+            product.id_brand = undefined
+        return res.json({
+            code: 200,
+            status: 'OK',
+            data: product
+        })
+    } catch (err) {
+        return res.json({
+            code: 500,
+            status: 'Internal Error',
+            message: 'Something wnet wrong'
+        })
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    try {
+        const id_product = req.params.id;
+        const existProduct = await Product.findByPk(id_product)
+        if (existProduct !== null) {
+            await existProduct.destroy()
+            return res.json({
+                code: 200,
+                status: 'Deleted',
+                message: 'Successfully'
+            })
+        } else {
+            return res.json({
+                code: 400,
+                status: 'Bad Request',
+                message: 'Product does not exist'
             })
         }
     } catch (err) {
         console.log(err)
         res.json({
-            code : 500,
-            status : 'Internal Error',
-            message : 'Something went wrong'
+            code: 500,
+            status: 'Internal Error',
+            message: 'Something went wrong'
         })
     }
 }
 
-const updateProductInfo = async (req,res) => {
+const updateProductInfo = async (req, res) => {
     try {
-        const id  = req.params.id
+        const id = req.params.id
         const product = await Product.findByPk(id)
-        if(product !== null) {
-            const { name , price , description , id_brand , id_category } = req.body
+        if (product !== null) {
+            const { name, price, description, id_brand, id_category } = req.body
 
             const brand = await Brand.findByPk(id_brand)
             const category = await Category.findByPk(id_category)
 
-            if(brand !== null && category !== null) {
+            if (brand !== null && category !== null) {
                 product.update({
-                    name : name,
-                    price : price,
-                    description : description,
-                    id_brand : brand.id,
-                    id_category : category.id
+                    name: name,
+                    price: price,
+                    description: description,
+                    id_brand: brand.id,
+                    id_category: category.id
                 })
 
                 return res.json({
-                    code : 200,
-                    status : 'Updated',
-                    message : 'Sucessfully'
+                    code: 200,
+                    status: 'Updated',
+                    message: 'Sucessfully'
                 })
             } else {
                 return res.json({
-                    code : 400,
-                    status : 'Bad Request',
-                    message : 'Product must have both of the brand and category'
+                    code: 400,
+                    status: 'Bad Request',
+                    message: 'Product must have both of the brand and category'
                 })
             }
-        } else if(product === null) {
+        } else if (product === null) {
             return res.json({
-                code : 400,
-                status : 'Bad Request',
-                message : 'Product does not exist'
+                code: 400,
+                status: 'Bad Request',
+                message: 'Product does not exist'
             })
         }
     } catch (err) {
         return res.json({
-            code : 500,
-            status : 'Internal Error',
-            message : 'Something went wrong'
+            code: 500,
+            status: 'Internal Error',
+            message: 'Something went wrong'
         })
     }
 }
 
-const getAllProduct = async (req,res) => {
+const getAllProduct = async (req, res) => {
     try {
         const { page } = req.query
         const { count } = await Product.findAndCountAll();
 
-        let listProduct;
-
-        if(count <= 7) {
-            listProduct = await Product.findAndCountAll({ include : { all : true } }, {
-                limit : 7,
-                offset : 0
+        let listProduct = []
+        let data;
+        if (count <= 7) {
+            data = await Product.findAll({
+                limit: 7,
+                offset: 0
             })
         } else {
-            listProduct = await Product.findAndCountAll({ include : { all : true } },{
-                limit : ((count - page*7) > 0) ? 7 : count%7,
-                offset : ((count - page*7) > 0) ? count - page*7 : 0
+            data = await Product.findAll({
+                limit: ((count - page * 7) > 0) ? 7 : count % 7,
+                offset: ((count - page * 7) > 0) ? count - page * 7 : 0
+            })
+            for (var i = 0; i < data.length; i++) {
+                let category = await data[i].getCategory()
+                let brand = await data[i].getBrand()
+                data[i].id_brand = data[i].id_category = undefined
+                var plain = await data[i].get({ plain: true })
+                plain['category'] = await category.get({ plain: true })
+                plain['brand'] = await brand.get({ plain: true })
+                listProduct.push(plain)
+            }
+            return res.json({
+                code: 200,
+                status: 'OK',
+                totalPage: Math.ceil(count / 7),
+                data: listProduct.reverse()
             })
         }
-        listProduct.rows.forEach((product) => {
-            product.id_brand = product.id_category = undefined
-        })
-        return res.json({
-            code : 200,
-            status : 'OK',
-            totalPage : Math.ceil(count/7),
-            data : listProduct.rows.reverse()
-        })
     } catch (err) {
+        console.log(err)
         return res.json({
-            code : 500,
-            status : 'Internal Error',
-            message : 'Something went wrong' 
+            code: 500,
+            status: 'Internal Error',
+            message: 'Something went wrong'
         })
     }
 }
 
-const activeProduct = async (req,res) => {
+const activeProduct = async (req, res) => {
     try {
         const { id_product } = req.params
 
         const product = await Product.findByPk(id_product)
 
-        if(product !== null) {
-            product.update({ active : true })
+        if (product !== null) {
+            product.update({ active: true })
             return res.json({
-                code : 200,
-                status : 'Actived',
-                message : 'Successfully'
+                code: 200,
+                status: 'Actived',
+                message: 'Successfully'
             })
         } else {
             return res.json({
-                code : 400,
-                status : 'Bad Request',
-                message : 'Product does not exist'
+                code: 400,
+                status: 'Bad Request',
+                message: 'Product does not exist'
             })
         }
     } catch (err) {
         return res.json({
-            code : 500,
-            status : 'Interal Error',
-            message : 'Something went wrong'
+            code: 500,
+            status: 'Interal Error',
+            message: 'Something went wrong'
         })
     }
 }
 
 
-const getAllProductForCustomer = async (req,res) => {
+const getAllProductForCustomer = async (req, res) => {
     try {
         const { page } = req.query
-        const { count } = await Product.findAndCountAll({ where : { active : true } });
+        const { count } = await Product.findAndCountAll();
 
-        let listProduct;
-
-        if(count <= 7) {
-            listProduct = await Product.findAndCountAll({ where : { active : true } , include : { all : true } }, {
-                limit : 7,
-                offset : 0
+        let listProduct = []
+        let data;
+        if (count <= 7) {
+            data = await Product.findAll({ where: { active: true } }, {
+                limit: 7,
+                offset: 0
             })
         } else {
-            listProduct = await Product.findAndCountAll({ where : { active : true } , include : { all : true } },{
-                limit : ((count - page*7) > 0) ? 7 : count%7,
-                offset : ((count - page*7) > 0) ? count - page*7 : 0
+            data = await Product.findAll({ where: { active: true } }, {
+                limit: ((count - page * 7) > 0) ? 7 : count % 7,
+                offset: ((count - page * 7) > 0) ? count - page * 7 : 0
+            })
+            for (var i = 0; i < data.length; i++) {
+                let category = await data[i].getCategory()
+                let brand = await data[i].getBrand()
+                data[i].id_brand = data[i].id_category = undefined
+                var plain = await data[i].get({ plain: true })
+                plain['category'] = await category.get({ plain: true })
+                plain['brand'] = await brand.get({ plain: true })
+                listProduct.push(plain)
+            }
+            return res.json({
+                code: 200,
+                status: 'OK',
+                totalPage: Math.ceil(count / 7),
+                data: listProduct.reverse()
             })
         }
-        listProduct.rows.forEach((product) => {
-            product.id_brand = product.id_category = undefined
-        })
-        return res.json({
-            code : 200,
-            status : 'OK',
-            totalPage : Math.ceil(count/7),
-            data : listProduct.rows.reverse()
-        })
     } catch (err) {
         return res.json({
-            code : 500,
-            status : 'Internal Error',
-            message : 'Something went wrong' 
+            code: 500,
+            status: 'Internal Error',
+            message: 'Something went wrong'
         })
     }
 }
--- src/model/product.model.js
+++ src/model/product.model.js
@@ -2,66 +2,92 @@ const { DataTypes } = require('sequelize')
 const db = require('../../utils/db');
 const Brand = require('../model/brands.model')
 const Category = require('../model/categories.model')
+const extensionArr = ['jpg', 'jpeg', 'png']
 
 const Product = db.define('tbl_products', {
-    id : {
-        type : DataTypes.INTEGER,
-        allowNull : false,
-        primaryKey : true,
-        autoIncrement : true,
-        field : 'id_product'
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        field: 'id_product'
     },
-    name : {
-        type : DataTypes.STRING,
-        allowNull : false,
-        validate : {
-            notEmpty : { msg : 'Product must not be empty' },
-            notNull : { msg : 'Product must have a name' }
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: { msg: 'Product must not be empty' },
+            notNull: { msg: 'Product must have a name' }
         }
     },
-    price : {
-        type : DataTypes.INTEGER,
-        allowNull : false,
-        validate : {
-            isInt : { msg : 'Price can only be an integer ' },
-            notEmpty : { msg : 'Product must have a price' }
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            isInt: { msg: 'Price can only be an integer ' },
+            notEmpty: { msg: 'Product must have a price' }
         }
     },
-    quantity : {
-        type : DataTypes.INTEGER,
-        allowNull : false,
-        defaultValue : 0
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
     },
-    description : {
-        type : DataTypes.STRING,
-        allowNull : true
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true
     },
-    image1 : {
-        type : DataTypes.STRING,
-        allowNull : true
+    image1: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+            checkExtension(value) {
+                let [filename, extension] = value.split('.', 2)
+                if (!extensionArr.includes(extension)) {
+                    throw new Error('Invalid image format')
+                }
+            }
+        }
     },
-    image2 : {
-        type : DataTypes.STRING,
-        allowNull : true
+    image2: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+            checkExtension(value) {
+                let [filename, extension] = value.split('.', 2)
+                if (!extensionArr.includes(extension)) {
+                    throw new Error('Invalid image format')
+                }
+            }
+        }
     },
-    image3 : {
-        type : DataTypes.STRING,
-        allowNull : true
+    image3: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+            checkExtension(value) {
+                let [filename, extension] = value.split('.', 2)
+                if (!extensionArr.includes(extension)) {
+                    throw new Error('Invalid image format')
+                }
+            }
+        }
     },
-    active :{
+    active: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: false
     }
 
 })
-  // add foreign key
-Brand.hasMany(Product, {foreignKey : 'id_brand', as : 'products'})
-Product.belongsTo(Brand, { foreignKey : 'id_brand' , as : 'brand' })
+// add foreign key
+Brand.hasMany(Product, { foreignKey: 'id_brand', as: 'products' })
+Product.belongsTo(Brand, { foreignKey: 'id_brand', as: 'brand' })
 
-Category.hasMany(Product, { foreignKey : 'id_category', as : 'products'})
-Product.belongsTo(Category, { foreignKey : 'id_category', as : 'category' })
+Category.hasMany(Product, { foreignKey: 'id_category', as: 'products' })
+Product.belongsTo(Category, { foreignKey: 'id_category', as: 'category' })
 
+db.sync({alter : true})
 
 module.exports = Product;
 
