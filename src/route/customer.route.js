ea2ee9a0f77146ce7c7f64a56a317339e22d9653
Nguyen Tran Hoang An <hoangan726@gmail.com>
2021-11-01 20:34:07 +0700

    Adding route geting all brand and category

--- src/route/customer.route.js
+++ src/route/customer.route.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const controller = require('../controller/product.controller')
+const productController = require('../controller/product.controller')
+const categoryController = require('../controller/categories.controller')
+const brandController = require('../controller/brand.controller')
 
 const router = express.Router()
 
-router.get('/product',controller.getAllProductForCustomer)
-router.get('/product/filter', controller.filterProduct)
-router.get('/product/:id', controller.getProductById)
+router.get('/product',productController.getAllProductForCustomer)
+router.get('/product/filter', productController.filterProduct)
+router.get('/product/:id', productController.getProductById)
+router.get('/category', categoryController.selectCategoryOption)
+router.get('/brand', brandController.selectBrandOption)
 
 module.exports = router;
