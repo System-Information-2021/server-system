23061a9cbfa307f4507a39982232717d02ca504d
Thanh Linh <linh.thanh@wonderlabs.io>
2021-11-10 19:19:44 +0700

    change file index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors');
-const db = require('./utils/db')
-const path = require('path')
 
 const app = express()
 require('dotenv').config()
@@ -18,10 +16,10 @@ app.use(cors({
 // Define routes 
 const userRoute = require('./src/route/user.route')
 const categoriesRoute = require('./src/route/categories.route')
-const brandRoute= require('./src/route/brands.route')
+const brandRoute = require('./src/route/brands.route')
 const productRoute = require('./src/route/products.route')
 const customerRoute = require('./src/route/customer.route')
-const cartRoute= require('./src/route/cart.route')
+const cartRoute = require('./src/route/cart.route')
 
 // Use middleware 
 app.use(bodyParser.json()) // for parsing application/json
@@ -30,19 +28,19 @@ app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-
 
 app.use('/', serviceRoute)
 
-app.use('/user', authorization , userRoute)
+app.use('/user', authorization, userRoute)
 
-app.use('/category', authorization , categoriesRoute)
+app.use('/category', authorization, categoriesRoute)
 
-app.use('/brand', authorization ,brandRoute)
+app.use('/brand', authorization, brandRoute)
 
-app.use('/product', authorization ,productRoute )
+app.use('/product', authorization, productRoute)
 
 app.use('/customer', customerRoute)
 
-app.use('/cart',authorization,cartRoute);
+app.use('/cart', authorization, cartRoute);
 
-app.listen(process.env.PORT, async () => {
+app.listen(process.env.PORT || 3000, async () => {
   console.log(`Server is listening at http://localhost:${process.env.PORT}`)
 })
 
